304910467206db57cc8553c95af8421337bd3871
Diego Carvallo <dccakes@gmail.com>
2024-04-22 10:49:20 +0100

    add secrets

--- function_app.py
+++ function_app.py
@@ -1,6 +1,19 @@
 import azure.functions as func
 import logging
 
+from azure.identity import DefaultAzureCredential
+from azure.keyvault.secrets import SecretClient
+
+def get_secret(key_vault_name: str, key_name: str) -> tuple:
+    credential = DefaultAzureCredential()
+    key_client = SecretClient(
+        vault_url=f"https://{key_vault_name}.vault.azure.net/", credential=credential
+    )
+    secret = key_client.get_secret(key_name)
+
+    return secret.properties.content_type, secret.value
+
+
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 @app.route(route="httpQuick")
@@ -27,6 +40,9 @@ def httpQuick(req: func.HttpRequest) -> func.HttpResponse:
 @app.route(route="httpSlow")
 def httpSlow(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
+    key_vault = "kv-eus-dev-ndg001"
+    secret_name = "sampleSecret"
+    content_type, secret = get_secret(key_vault, secret_name)
 
     name = req.params.get('name')
     if not name:
@@ -41,6 +57,6 @@ def httpSlow(req: func.HttpRequest) -> func.HttpResponse:
         return func.HttpResponse(f"Hello, {name}. This SLOW HTTP triggered function executed successfully.")
     else:
         return func.HttpResponse(
-             "This SLOW HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             f"This SLOW HTTP triggered function executed successfully. secret: {secret}, content: {content_type} Pass a name in the query string or in the request body for a personalized response.",
              status_code=200
         )
