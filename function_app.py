9aba377f55ff5a5f2793f4eb6a8cab6be444ebad
Diego Carvallo <dccakes@gmail.com>
2024-04-22 11:05:51 +0100

    add health endpoint

--- function_app.py
+++ function_app.py
@@ -1,17 +1,17 @@
 import azure.functions as func
 import logging
 
-from azure.identity import DefaultAzureCredential
-from azure.keyvault.secrets import SecretClient
+#from azure.identity import DefaultAzureCredential
+#from azure.keyvault.secrets import SecretClient
 
-def get_secret(key_vault_name: str, key_name: str) -> tuple:
-    credential = DefaultAzureCredential()
-    key_client = SecretClient(
-        vault_url=f"https://{key_vault_name}.vault.azure.net/", credential=credential
-    )
-    secret = key_client.get_secret(key_name)
-
-    return secret.properties.content_type, secret.value
+#def get_secret(key_vault_name: str, key_name: str) -> tuple:
+#    credential = DefaultAzureCredential()
+#    key_client = SecretClient(
+#        vault_url=f"https://{key_vault_name}.vault.azure.net/", credential=credential
+#    secret = key_client.get_secret(key_name)
+#    )
+#
+#    return secret.properties.content_type, secret.value
 
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
@@ -42,7 +42,8 @@ def httpSlow(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
     key_vault = "kv-eus-dev-ndg001"
     secret_name = "sampleSecret"
-    content_type, secret = get_secret(key_vault, secret_name)
+    content_type, secret = "taco", "bell"
+    #content_type, secret = get_secret(key_vault, secret_name)
 
     name = req.params.get('name')
     if not name:
@@ -58,5 +59,12 @@ def httpSlow(req: func.HttpRequest) -> func.HttpResponse:
     else:
         return func.HttpResponse(
              f"This SLOW HTTP triggered function executed successfully. secret: {secret}, content: {content_type} Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
+    
+@app.route(route="health")
+def health(req: func.HttpRequest) -> func.HttpResponse:
+    return func.HttpResponse(
+             "OK",
              status_code=200
         )
